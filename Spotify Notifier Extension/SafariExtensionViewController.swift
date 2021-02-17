3cd2924a4ef795a5b31a02ae9827380b1439561e
Ryan Mohta <ryanmohta@gmail.com>
2021-02-17 02:57:30 -0800

    Initial Commit

---
+++ Spotify Notifier Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  Spotify Notifier Extension
+//
+//  Created by Ryan Mohta on 2/17/21.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
