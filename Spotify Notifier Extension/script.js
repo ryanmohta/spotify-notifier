2c604985a33080e2a11381b483f289f9c9c2b1d8
Ryan Mohta <ryanmohta@gmail.com>
2021-02-22 20:24:59 -0800

    Added image file writing

--- Spotify Notifier Extension/script.js
+++ Spotify Notifier Extension/script.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import axios from 'axios';
+
 document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(() => {
         // Initial target inner node
@@ -5,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var targetNodeInner = document.querySelector('.now-playing');
         var observerConfig = { attributes: true, childList: true, characterData: true };
         innerObserver.observe(targetNodeInner, observerConfig);
-        
+
         var outerObserver = new MutationObserver((mutations) => {
             onMutated(mutations);
             var innerObserver = new MutationObserver(onMutated);
@@ -13,26 +16,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
             var observerConfig = { attributes: true, childList: true, characterData: true };
             innerObserver.observe(targetNodeInner, observerConfig);
         });
-        
+
         var targetNodeOuter = document.querySelector('.now-playing-bar__left');
         outerObserver.observe(targetNodeOuter, observerConfig);
-        
-        
+
+
         function onMutated(mutations) {
             mutations.forEach(function(mutation) {
                 let title = document.querySelector('.now-playing ._3773b711ac57b50550c9f80366888eab-scss');
                 let artist = document.querySelector('.now-playing .b6d18e875efadd20e8d037931d535319-scss');
                 let albumCover = document.querySelector('.now-playing ._64acb0e26fe0d9dff68a0e9725b2a920-scss');
-                
+
                 if (title !== null && artist !== null && albumCover !== null) {
                     title = title.innerText;
                     artist = artist.innerText;
                     albumCover = albumCover.src;
-                    
+
                     safari.extension.dispatchMessage("Song Changed", { "title": title, "artist": artist, "albumCover": albumCover });
                 }
             });
         }
-        
+
     }, 5000);
 });
+
+function writeToFile(url) {
+  const startingIndex = url.indexOf('/images/') + 8;
+  const path = `images/${url.substring(startingIndex)}`;
+  axios.get(url, { responseType:'stream' }).then((res) => {
+    res.data.pipe(fs.createWriteStream(path))
+    .on('close', function() {
+      console.log(`file with url ${url} written to path ${path}`);
+    });
+  })
+}
