16554b84b908c2b27beef51d03c14b1ceb9ca14f
Ryan Mohta <ryanmohta@gmail.com>
2021-02-22 19:33:41 -0800

    Updated mutation observer

--- Spotify Notifier Extension/Resources/script.js
+++ Spotify Notifier Extension/Resources/script.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(() => {
-        var observer = new MutationObserver(function(mutations) {
+        // Initial target inner node
+        var innerObserver = new MutationObserver(onMutated);
+        var targetNodeInner = document.querySelector('.now-playing');
+        var observerConfig = { attributes: true, childList: true, characterData: true };
+        innerObserver.observe(targetNodeInner, observerConfig);
+        
+        var outerObserver = new MutationObserver((mutations) => {
+            onMutated(mutations);
+            var innerObserver = new MutationObserver(onMutated);
+            var targetNodeInner = document.querySelector('.now-playing');
+            var observerConfig = { attributes: true, childList: true, characterData: true };
+            innerObserver.observe(targetNodeInner, observerConfig);
+        });
+        
+        var targetNodeOuter = document.querySelector('.now-playing-bar__left');
+        outerObserver.observe(targetNodeOuter, observerConfig);
+        
+        
+        function onMutated(mutations) {
             mutations.forEach(function(mutation) {
                 let title = document.querySelector('.now-playing ._3773b711ac57b50550c9f80366888eab-scss');
                 let artist = document.querySelector('.now-playing .b6d18e875efadd20e8d037931d535319-scss');
@@ -13,20 +31,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     
                     safari.extension.dispatchMessage("Song Changed", { "title": title, "artist": artist, "albumCover": albumCover });
                 }
-
             });
-        });
-         
-        // Notify me of everything!
-        var observerConfig = {
-            attributes: true,
-            childList: true,
-            characterData: true
-        };
-         
-        // Node, config
-        // In this case we'll listen to all changes to body and child nodes
-        var targetNode = document.querySelector('.now-playing-bar__left');
-        observer.observe(targetNode, observerConfig);
+        }
+        
     }, 5000);
 });
